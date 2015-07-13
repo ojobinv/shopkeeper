4f2017b75954f84f935acb2e95eed424bf3b73da
jobin <nidocojobinv@gmail.com>
2015-07-13 20:52:19 +0530

    Third Commit

--- src/main/java/dev/ovj/shopkeeper/controller/IndexController.java
+++ src/main/java/dev/ovj/shopkeeper/controller/IndexController.java
@@ -2,6 +2,7 @@ package dev.ovj.shopkeeper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -10,6 +11,11 @@ public class IndexController {
 	public String index(){
 		System.out.println("/WEB-INF/jsp/index.jsp");
 		return "/WEB-INF/jsp/index.jsp";
+	}
+	
+	@RequestMapping("/index.json")  
+	 public @ResponseBody String sendJSON(){
+		return new String("Json result");
 	}
 	
 }
