33e23246cc813cb6fc7556f7bc42f14251fe4f38
jobin <nidocojobinv@gmail.com>
2015-07-14 21:44:06 +0530

    Added Embedded dB and Added entities

---
+++ src/main/java/dev/ovj/shopkeeper/entity/Role.java
@@ -0,0 +1,46 @@
+package dev.ovj.shopkeeper.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String role_name;
+
+	@ManyToMany(mappedBy="roles")
+	private List<User> users;
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRole_name() {
+		return role_name;
+	}
+
+	public void setRole_name(String role_name) {
+		this.role_name = role_name;
+	}
+
+}
