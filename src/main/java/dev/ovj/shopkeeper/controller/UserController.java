56aab9251dd0ea9e955a5aec74edc62ea453f5a6
jobin <nidocojobinv@gmail.com>
2015-07-15 21:46:05 +0530

    15/07

--- src/main/java/dev/ovj/shopkeeper/controller/UserController.java
+++ src/main/java/dev/ovj/shopkeeper/controller/UserController.java
@@ -1,12 +1,20 @@
 package dev.ovj.shopkeeper.controller;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
+import javax.print.PrintException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import dev.ovj.shopkeeper.entity.User;
 import dev.ovj.shopkeeper.service.UserService;
@@ -19,8 +27,20 @@ public class UserController {
 	
 	@RequestMapping("/users")
 	public String users(Model model){
-		model.addAttribute("users", userService.findAll());
-		return model.toString();
+		List<User> users = userService.findAll();
+		model.addAttribute("users", users);
+		Gson gson = new Gson();
+		String json;
+		/*for(User user: users){
+			json = gson.toJson(user);
+			System.out.println(json);			
+		}*/
+		
+		Type listType = new TypeToken<List<User>>() {}.getType();
+		json = gson.toJson(users, listType);
+		System.out.println(json);
+		
+		return users.toString();
 	}
 	
 	/*@RequestMapping("/users")
